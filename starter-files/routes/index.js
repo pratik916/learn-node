fa086bdbd2e764b4075f9a730df098bf580cc9d1
Pratik Soni <pratiksoni@Pratiks-MacBook-Pro.local>
2018-02-26 01:43:42 +0530

    Learned templating with pug.

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log("Hey!!!!!");
+  const pratik = { name: 'Pratik', age:28, cool:true };
+  // res.send('Hey! It works!');
+  // res.json(pratik);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+  	name: 'Champavat',
+  	dog: 'Sarpanch'
+  });
 });
+
+router.get('/reverse/:name', (req, res) => {
+	// res.send("It Works");
+	const reverse = [...req.params.name].reverse().join('');
+	// res.send(req.params.name);
+	res.send(reverse);
+})
 
 module.exports = router;
